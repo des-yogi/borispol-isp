aef55e401876c8b03a59a8b3f16e759237d04ab7
des-yogi <nuevo-design@mail.ru>
2018-02-16 18:40:44 +0200

    index markup in progress

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,7 +1,76 @@
 // Если на проекте jQuery
-// $( document ).ready(function() {
-//   // code
-// });
+/*$( document ).ready(function() {
+ code
+});*/
+
+$(document).ready(function(){
+
+  $("#hero-slider").owlCarousel({
+    items: 1,
+    loop: true,
+    // autoplay: true,
+    // autoplayTimeout: 3000,
+    // autoplayHoverPause: true,
+    responsive:{
+        0:{
+          nav: false
+        },
+        768:{
+          nav: false
+        },
+        1200:{
+          nav: true
+        }
+    }
+  });
+
+  $("#actions-slider").owlCarousel({
+    items: 1,
+    loop: true,
+    // autoplay: true,
+    // autoplayTimeout: 3000,
+    // autoplayHoverPause: true,
+    responsive:{
+        0:{
+          nav: false
+        },
+        768:{
+          items: 2,
+          margin: 30,
+          nav: false
+        },
+        1200:{
+          items: 3,
+          margin: 30,
+          nav: false
+        }
+    }
+  });
+
+  $("#news-slider").owlCarousel({
+    items: 1,
+    loop: true,
+    // autoplay: true,
+    // autoplayTimeout: 3000,
+    // autoplayHoverPause: true,
+    responsive:{
+        0:{
+          nav: false
+        },
+        768:{
+          items: 2,
+          margin: 30,
+          nav: false
+        },
+        1200:{
+          items: 4,
+          margin: 30,
+          nav: false
+        }
+    }
+  });
+
+});
 
 // Изоляция без jQuery
 // (function(){
