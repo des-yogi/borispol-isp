1194e462874db4ee259278abd5c38f37db013c7e
des-yogi <nuevo-design@mail.ru>
2018-02-20 19:20:36 +0200

    Index in progress. Action slider - finished

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -31,19 +31,19 @@ $(document).ready(function(){
     // autoplayTimeout: 3000,
     // autoplayHoverPause: true,
     responsive:{
-        0:{
-          nav: false
-        },
-        768:{
-          items: 2,
-          margin: 30,
-          nav: false
-        },
-        1200:{
-          items: 3,
-          margin: 30,
-          nav: false
-        }
+      0:{
+        nav: false
+      },
+      768:{
+        items: 2,
+        margin: 30,
+        nav: false
+      },
+      1200:{
+        items: 3,
+        margin: 30,
+        nav: false
+      }
     }
   });
 
@@ -54,23 +54,42 @@ $(document).ready(function(){
     // autoplayTimeout: 3000,
     // autoplayHoverPause: true,
     responsive:{
-        0:{
-          nav: false
-        },
-        768:{
-          items: 2,
-          margin: 30,
-          nav: false
-        },
-        1200:{
-          items: 4,
-          margin: 30,
-          nav: false
-        }
+      0:{
+        nav: false
+      },
+      768:{
+        items: 2,
+        margin: 30,
+        nav: false
+      },
+      1200:{
+        items: 4,
+        margin: 30,
+        nav: false
+      }
     }
   });
 
 });
+
+(function(){
+  // Анимация панели телефонов
+  var phoneClickPlace = document.querySelector('.user-nav__phone-list');
+  var screenWidth = document.documentElement.clientWidth;
+
+  if (phoneClickPlace) {
+    phoneClickPlace.addEventListener('click', function (e) {
+      if ( !phoneClickPlace.classList.contains('user-nav__phone-list--open') ) {
+        phoneClickPlace.classList.add('user-nav__phone-list--open');
+        phoneClickPlace.classList.remove('user-nav__phone-list--close');
+      } else {
+          phoneClickPlace.classList.add('user-nav__phone-list--close');
+          phoneClickPlace.classList.remove('user-nav__phone-list--open');
+      }
+    });
+  }
+
+}());
 
 // Изоляция без jQuery
 // (function(){
